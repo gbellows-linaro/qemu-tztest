06564ab7413a5f02fa1b6c4d8b19dd52ed566633
Greg Bellows <greg.bellows@linaro.org>
2014-07-28 19:06:45 -0500

    fixup! Add platform specific directory and header
    Signed-off-by: Greg Bellows <greg.bellows@linaro.org>

--- platform/vexpress/platform.h
+++ platform/vexpress/platform.h
@@ -1,12 +1,12 @@
-#ifndef _ARCH_H
-#define _ARCH_H
+#ifndef _PLATFORM_H
+#define _PLATFORM_H
 
 /* NORFLASH0 on Vexpress aseries (a15) is mapped from 0x08000000 to 0x0A000000
  * which is 32MB.  It is also aliased to 0x0 (to 0x2000000).
  * It is acceptable to have the text here as it is RO.
  */
 #define FLASH_SEC_BASE 0x00000000
-#define FLASH_NSEC_BASE 0x10000
+#define FLASH_NSEC_BASE 0x00010000
 
 #define RAM_SEC_BASE 0x80000000
 #define RAM_NSEC_BASE 0x88000000
