3ed3ede2a3efd86789982286993282f29bb3a030
Greg Bellows <greg.bellows@linaro.org>
2014-09-17 15:45:05 -0700

    Add virt platform support
    Added platform.h header containing QEMU virt machine constants.
    
    Signed-off-by: Greg Bellows <greg.bellows@linaro.org>

---
+++ platform/virt/platform.h
@@ -0,0 +1,33 @@
+#ifndef _PLATFORM_H
+#define _PLATFORM_H
+
+/* NORFLASH0 on Vexpress aseries (a15) is mapped from 0x08000000 to 0x0A000000
+ * which is 32MB.  It is also aliased to 0x0 (to 0x2000000).
+ * It is acceptable to have the text here as it is RO.
+ */
+#define FLASH_SEC_BASE 0x00000000
+#define FLASH_NSEC_BASE 0x00010000
+
+#define RAM_SEC_BASE 0x40000000
+#define RAM_NSEC_BASE 0x48000000
+
+#define UART0_BASE 0x09000000
+
+/* Taken from Linux vexpress support */
+#define SYSREG_BASE 0x1c010000
+#define SYSREG_CFGCTRL 0xa4
+#define SYS_CFGCTRL_FUNC(n) (((n) & 0x3f) << 20)
+#define SYS_CFGFUNC_RESET 5
+#define SYS_CFGFUNC_SHUTDOWN 8
+#define SYS_CFGCTRL_START       (1 << 31)
+#define SYS_CFGCTRL_WRITE       (1 << 30)
+#define SYS_SHUTDOWN    (SYS_CFGCTRL_FUNC(SYS_CFGFUNC_SHUTDOWN) | \
+                         SYS_CFGCTRL_START | SYS_CFGCTRL_WRITE)
+#define SYS_RESET       (SYS_CFGCTRL_FUNC(SYS_CFGFUNC_SHUTDOWN) | \
+                         SYS_CFGCTRL_START | SYS_CFGCTRL_WRITE)
+
+#define STACK_SIZE 0x4000
+
+#define TZTEST_COMMON_HEAP_BASE 0x49000000
+#define TZTEST_SHARED_HEAP_BASE 0x4A000000
+#endif
