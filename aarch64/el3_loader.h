fa0c1116fcc0cd32ad2f524f1bb677a0364d1367
Greg Bellows <greg.bellows@linaro.org>
2015-02-24 11:37:45 -0600

    Enable EL3 MMU
    Signed-off-by: Greg Bellows <greg.bellows@linaro.org>

--- aarch64/el3_loader.h
+++ aarch64/el3_loader.h
@@ -1,10 +1,10 @@
 #ifndef _EL3_LOADER_H
 #define _EL3_LOADER_H
 
-extern uintptr_t _EL3_RAM_TEXT;
-uintptr_t EL3_RAM_TEXT = (uintptr_t)&_EL3_RAM_TEXT;
-extern uintptr_t _EL3_RAM_DATA;
-uintptr_t EL3_RAM_DATA = (uintptr_t)&_EL3_RAM_DATA;
+extern uintptr_t _EL3_TEXT_BASE;
+uintptr_t EL3_TEXT_BASE = (uintptr_t)&_EL3_TEXT_BASE;
+extern uintptr_t _EL3_DATA_BASE;
+uintptr_t EL3_DATA_BASE = (uintptr_t)&_EL3_DATA_BASE;
 extern uintptr_t _EL3_TEXT_SIZE;
 uint64_t EL3_TEXT_SIZE = (uint64_t)&_EL3_TEXT_SIZE;
 extern uintptr_t _EL3_DATA_SIZE;
