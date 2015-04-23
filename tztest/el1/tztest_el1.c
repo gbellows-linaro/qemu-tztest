73d906eb334851c4c4600163f667277e33360aeb
Greg Bellows <greg.bellows@linaro.org>
2015-04-23 16:47:11 -0500

    Make EL1 scr validation test aarch32 only
    Had to make the EL1 SMC validation test aarch32 only due to the issue where
    setting the SCR.SMD bit in the test prevents returning to EL3 to clear it.
    This is not an issue on aarch32 in QEMU because the writes are ignored unless
    virt is present.
    
    Signed-off-by: Greg Bellows <greg.bellows@linaro.org>

--- tztest/el1/tztest_el1.c
+++ tztest/el1/tztest_el1.c
@@ -32,28 +32,19 @@ uint32_t el1_check_smc(uint32_t __attribute__((unused))arg)
 
     SMC_GET_REG(SCR, 3, scr);
     SMC_SET_REG(SCR, 3, scr | SCR_SMD);
-#ifdef AARCH64
-    /* On AArch64, SMC calls below EL3 should result in an undefined exception
-     * if the SCR.SMD bit is set.  This is regardless of whether EL2 is present
-     * or not.
-     */
-    TEST_MSG("SMC call without virt (SCR.SMD = 1)");
-    TEST_EL3_EXCEPTION(__smc(SMC_OP_NOOP, NULL), EC_UNKNOWN);
-#else
-    /* On AArch32, SMC calls are undefined when SCR.SCD is set only when the
-     * virtualization extensions are present.
-     */
-    //TEST_EL1_EXCEPTION(__smc(SMC_OP_NOOP, NULL), EC_UNKNOWN);
 
     /* When the virtualization extensions are not present, the SCR.SCD bit
      * setting should have no impact on SMC.
+     * We have to call with our structured smc call rather than an asm as it is
+     * expected to go through so the arguments have to be set-up properly.
      */
     TEST_MSG("SMC call without virt (SCR.SCD = 1)");
     TEST_NO_EXCEPTION(__smc(SMC_OP_NOOP, NULL));
 
-#endif
-
-    /* Restor SCR */
+    /* Restore SCR
+     * Note: This will cause an exception if the above write of the SMD bit
+     * succeeds making it impossible to get back to EL3.
+     */
     SMC_SET_REG(SCR, 3, scr);
 
     return 0;
